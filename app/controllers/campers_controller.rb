e929f5479209bce095796f2e4fe4cc110cd22c59
Lorenzo Fiorista <lorenzo.fiorista@gmail.com>
2022-02-01 20:47:27 +0100

    new camper form

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -1,10 +1,27 @@
 class CampersController < ApplicationController
   def index
+    @campers = Camper.all
   end
 
   def new
+    #@user = User.find(params[:user_id])
+    @camper = Camper.new
   end
 
   def create
+    @camper = Camper.new(camper_params)
+    #@user = User.find(params[:user_id])
+    @camper.user = @user
+    if @camper.save
+      redirect_to camper_path(@camper)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def camper_params
+    params.require(:camper).permit(:model, :price, :description, :availability, :photo_url)
   end
 end
