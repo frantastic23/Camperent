fc45a7816bd4d1a5d78718e511d2124ba8676885
Marcy Hundschell Dela Cruz <marcy.delacruz@gmail.com>
2022-02-03 21:22:20 +0100

    Booking_summary

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :campers, only: %i[index new create show]
   resources :users do
-    resources :booking_requests, only: %i[new create destroy index show]
+    resources :booking_requests, only: %i[new create destroy index]
   end
+  resources :booking_requests, only: %i[show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
