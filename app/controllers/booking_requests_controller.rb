680f06edf3c0e46f1bb6f58c95221990314a25bf
Camilo Zamora <cazmera.music@gmail.com>
2022-02-03 18:46:48 +0100

    booking request updated

--- app/controllers/booking_requests_controller.rb
+++ app/controllers/booking_requests_controller.rb
@@ -4,6 +4,14 @@ class BookingRequestsController < ApplicationController
   end
 
   def create
+    @user = current_user
+    @camper = Camper.find(params[:camper_id])
+    @booking = BookingRequest.new(booking_request_params)
+  end
 
+  private
+
+  def booking_request_params
+    params.require(:booking_request).permit(:date_from, :date_to, :cost)
   end
 end
