d85f2b600176f4688354e7d5cdcf43d9c0b71644
Camilo Zamora <cazmera.music@gmail.com>
2022-02-04 01:24:19 +0100

    Added validations to model

--- app/models/booking_request.rb
+++ app/models/booking_request.rb
@@ -1,4 +1,13 @@
 class BookingRequest < ApplicationRecord
   belongs_to :user
   belongs_to :camper
+  validates :user, presence: true
+  validates :date_from, presence: true
+  validates :date_to, presence: true
+  validates :cost, presence: true
+  validates_date :date_from, on_or_after: -> { Date.current }
+  validates_date :date_from, after: -> { :date_from }
+  validates_each :camper do |record, attr, value|
+    record.errors.add(attr, 'camper is not available for booking') unless value.available?(:date_from, :date_to)
+  end
 end
