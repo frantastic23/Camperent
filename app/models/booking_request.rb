c9fea3b171b10e03aca69cba8a5e7616724bbac1
Camilo Zamora <cazmera.music@gmail.com>
2022-02-04 02:20:05 +0100

    validations of dates tested and operational

--- app/models/booking_request.rb
+++ app/models/booking_request.rb
@@ -5,8 +5,8 @@ class BookingRequest < ApplicationRecord
   validates :date_from, presence: true
   validates :date_to, presence: true
   validates :cost, presence: true
-  validates :date_from, date: { on_or_after: Date.current }
-  validates_date :date_to, date: { after: :date_from }
+  validates_date :date_from, on_or_after: -> { Date.current }, message: "date must be after today"
+  validates_date :date_to, after: -> { :date_from }, message: "date must be after the initial date"
   validates_each :camper do |record, attr, value|
     record.errors.add(attr, 'camper is not available for booking') unless value.available?(:date_from, :date_to)
   end
