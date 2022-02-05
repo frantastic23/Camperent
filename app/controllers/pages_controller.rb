869b735cf4835513cdfb23a7e765e06b9fb5e7f5
frantastic23 <francescacartia@yahoo.it>
2022-02-05 13:03:39 +0100

    alert and home fixed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
   before_action :authenticate_user!
   skip_before_action :authenticate_user!, only: :home, raise: false
   def home
+  end
+
+  def campers
     @campers = Camper.all
   end
 end
