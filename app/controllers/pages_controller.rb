9f8a1d2815bf36a92121b5b983858f45f79a9ecb
Camilo Zamora <cazmera.music@gmail.com>
2022-02-08 21:34:09 +0100

    added correct parameters

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,10 +5,9 @@ class PagesController < ApplicationController
   end
 
   def campers
-    if params[:dt].present? || params[:df].present? || params[:address].present?
-      camps = Camper.search_by_addres(params[:address])
-      @campers = camps.select {|camper| camper.available?(params[:df], params[:dt])}
-
+    if params[:date_from].present? || params[:date_to].present? || params[:address].present?
+      params[:address].present? ? camps = Camper.search_by_address(params[:address]) : camps = Camper.all
+      @campers = camps.select {|camper| camper.available?(params[:date_from], params[:date_to])}
     else
       @campers = Camper.all
     end
--- app/models/camper.rb
+++ app/models/camper.rb
@@ -4,7 +4,7 @@ class Camper < ApplicationRecord
   has_one_attached :photo
   include PgSearch::Model
   pg_search_scope :search_by_address,
-    against: [ :address],
+    against: [:address],
     using: {
       tsearch: { prefix: true }
     }
