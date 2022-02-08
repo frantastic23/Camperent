35e2f3ddb42c49f73c9f48995613bf10588d9571
Lorenzo Fiorista <lorenzo.fiorista@gmail.com>
2022-02-08 20:33:26 +0100

    add geocoder gem and did a little bit of setup for geocoding

--- app/models/camper.rb
+++ app/models/camper.rb
@@ -2,6 +2,8 @@ class Camper < ApplicationRecord
   belongs_to :user
   has_many :booking_requests, dependent: :destroy
   has_one_attached :photo
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
   # d - m - Y
   def available?(date_from, date_to)
