d7d3e540fae15d99fab7bada1e98e670e1348f9c
frantastic23 <francescacartia@yahoo.it>
2022-02-10 20:19:05 +0100

    resolving conflict

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,6 @@ class PagesController < ApplicationController
 
   def campers
     @campers = Camper.all
-<<<<<<< HEAD
     description(@campers)
   end
 
@@ -15,22 +14,20 @@ class PagesController < ApplicationController
   def description(items)
     items.map do |item|
       item.description = item.description[0, 150] if item.description.length > 150
-=======
-
-    @markers = @campers.geocoded.map do |camper|
-      {
-        lat: camper.latitude,
-        lng: camper.longitude,
-        info_window: render_to_string(partial: "info_window", locals: { camper: camper })
-      }
-    end
-
-    if params[:date_from].present? || params[:date_to].present? || params[:address].present?
-      params[:address].present? ? camps = Camper.search_by_address(params[:address]) : camps = Camper.all
-      @campers = camps.select {|camper| camper.available?(params[:date_from], params[:date_to])}
-    else
-      @campers = Camper.all
->>>>>>> 63e48bece59de349a1d648a9aad472a4f3a0c71b
     end
   end
-end
+
+  @markers = @campers.geocoded.map do |camper|
+    {
+      lat: camper.latitude,
+      lng: camper.longitude,
+      info_window: render_to_string(partial: "info_window", locals: { camper: camper })
+    }
+  end
+
+  if params[:date_from].present? || params[:date_to].present? || params[:address].present?
+    params[:address].present? ? camps = Camper.search_by_address(params[:address]) : camps = Camper.all
+    @campers = camps.select { |camper| camper.available?(params[:date_from], params[:date_to]) }
+  else
+    @campers = Camper.all
+  end
