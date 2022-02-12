4c41acc46ac4cffb9640795da78b1ab77ce51bca
Lorenzo Fiorista <lorenzo.fiorista@gmail.com>
2022-02-12 15:59:30 +0100

    user picture done (fingers crossed)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :campers, dependent: :destroy
   has_many :booking_requests, dependent: :destroy
+  has_one_attached :photo
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
