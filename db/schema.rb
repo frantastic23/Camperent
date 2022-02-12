1d9e751627530d6f44d551df02fd4616bae3f5bc
Camilo Zamora <cazmera.music@gmail.com>
2022-02-12 16:44:19 +0100

    last migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_12_134904) do
+ActiveRecord::Schema.define(version: 2022_02_12_140923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,12 +63,12 @@ ActiveRecord::Schema.define(version: 2022_02_12_134904) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "capacity"
-    t.string "fuel"
-    t.string "gear"
     t.string "address"
+    t.integer "capacity"
     t.float "latitude"
     t.float "longitude"
+    t.string "fuel"
+    t.string "gear"
     t.index ["user_id"], name: "index_campers_on_user_id"
   end
 
