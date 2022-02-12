41b3b70d8d935388cf79a28019a6485fc08cc920
frantastic23 <francescacartia@yahoo.it>
2022-02-12 13:37:37 +0100

    added validations

--- db/schema.rb
+++ db/schema.rb
@@ -89,11 +89,7 @@ ActiveRecord::Schema.define(version: 2022_02_12_123024) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+    t.index ["email"], name: "index_usersgit ents", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "booking_requests", "campers"
   add_foreign_key "booking_requests", "users"
