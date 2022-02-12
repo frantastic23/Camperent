75c64f12a5261a99959d60b6f70258bfc2491aa8
frantastic23 <francescacartia@yahoo.it>
2022-02-12 13:31:33 +0100

    migration for reviews

---
+++ db/migrate/20220212123024_add_content_to_review.rb
@@ -0,0 +1,5 @@
+class AddContentToReview < ActiveRecord::Migration[6.1]
+  def change
+    add_column :reviews, :content, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_12_104034) do
+ActiveRecord::Schema.define(version: 2022_02_12_123024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,8 @@ ActiveRecord::Schema.define(version: 2022_02_12_104034) do
   create_table "reviews", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "stars"
+    t.text "content"
   end
 
   create_table "users", force: :cascade do |t|
